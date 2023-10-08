0cae0517eaa20e0c0e241959fb7f5e0ff2e54db8
coderbri <coderbri23@gmail.com>
2023-10-07 23:56:02 -0400

    tested more test routes in hello_flask

--- Wk4-Flask/Lecture-Code/D8-Intro_to_Flask/hello_flask/server.py
+++ Wk4-Flask/Lecture-Code/D8-Intro_to_Flask/hello_flask/server.py
@@ -18,10 +18,21 @@ def say_hello_in_korean():
 def test_path_var( num ):
     return num # the number '7' will be returned on this URL
 
+@app.route('/hello/<name>')
+def hello_user( name ):
+    return f"Hello, {name}!"
+
+# * Type Converters
+@app.route('/hello/<name>/<int:num>')
+def repeat_hello( name, num ):
+    return f"<h2>Hello, {name * num}!</h2>"
+
+
 # * Render Templates
 @app.route('/home_page')
 def render_html():
     return render_template('index.html')
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
+# ? For Mac users, specify port=5001 as port=5000 is occupied for Apple AirPlay
