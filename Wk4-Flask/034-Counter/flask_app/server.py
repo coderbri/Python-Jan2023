f30aff5fa4274953c173fe1415343a8584056c03
coderbri <coderbri23@gmail.com>
2023-10-28 00:32:41 -0400

    cleaned up comments in server.py

--- Wk4-Flask/034-Counter/flask_app/server.py
+++ Wk4-Flask/034-Counter/flask_app/server.py
@@ -14,24 +14,17 @@ session.pop('key_name')		# clears a specific key
 
 @app.route('/')
 def index():
-    """
-    1. Have the root route render a template that displays todo the number of times the client has visited this site. Refresh the page several times to ensure the counter is working.
-    """
     if "count" not in session:
         session["count"] = 0
     else:
         session["count"] += 1
     
-    # * Adjust your code to display both how many times the user has actually visited the page, as well as the value of the counter, given the above functionality
     visit_count = session.get("visit_count", 0)
     session["visit_count"] = visit_count+1
     return render_template('index.html', visits=visit_count)
 
 @app.route('/increment_by_two')
 def increment_by_two():
-    """
-    2. NINJA BONUS: Add a +2 button underneath the counter and a new route that will increment the counter by 2
-    """
     if "count" not in session:
         session["count"] = 0
     else:
@@ -40,8 +33,6 @@ def increment_by_two():
 
 @app.route('/increment_custom', methods=['POST'])
 def increment_custom():
-    """5. Add a form that allows the user to specify the increment of the counter and have the counter increment accordingly
-    """
     increment_value = int(request.form.get('increment', 1))
     if "count" not in session:
         session["count"] = 0
@@ -51,15 +42,8 @@ def increment_custom():
 
 @app.route('/destroy_session')
 def reset():
-    """
-    3. Add a "/destroy_session" route that clears the session and redirects to the root route. Test it.
-    4. Add a Reset button to reset the counter
-
-    """
     session.clear()
     return redirect('/')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
